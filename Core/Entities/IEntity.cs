e035617f84a7a9a7f8e4adc63eba2850abbe3523
Fatma DURAK <86356645+fatmadurak@users.noreply.github.com>
2021-08-16 14:54:53 +0300

    Add files via upload

---
+++ Core/Entities/IEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+
+    //IEntity implement eden class bir veri tabanı tablosudur.
+    public interface IEntity
+    {
+
+
+    }
+}
